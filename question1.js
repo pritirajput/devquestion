49ce595bd0cd3174d5073a11e61d929148ac8f79
preetirajput <rajput.preeti.singh@gmail.com>
2022-03-06 20:33:13 +0530

    question 8 to 20 is done

--- question1.js
+++ question1.js
@@ -68,3 +68,62 @@ const FCC = "freeCodeCamp";
 let fact = "is cool!";
 fact = "is awesome!";
 console.log(FCC, fact); 
+
+//question 10 is Add Two Numbers with JavaScript
+const sum = 10 + 10;
+console.log("add : "+sum);
+
+//question 11 is Subtract One Number from Another with JavaScript
+const difference = 45 - 33;
+console.log("substract : "+difference);
+
+//question 12 is Multiply Two Numbers with JavaScript
+const product = 8 * 10;
+console.log("multiply : "+product);
+
+//question 13 is Divide One Number by Another with JavaScript
+const quotient = 66 / 33;
+console.log("divide : "+quotient);
+
+//question 14 is Increment a Number with JavaScript
+let myVar = 87;
+myVar++;
+myVar = myVar ++;
+console.log("increment value : "+myVar);
+
+//question 15 is Decrement a Number with JavaScript
+let MyVar = 11;
+MyVar--;
+console.log("decrement value : "+MyVar);
+
+//question 16 isCreate Decimal Numbers with JavaScript
+let myDecimal = 5.7;
+console.log("decimal number : "+myDecimal);
+
+//question 17 is Multiply Two Decimals with JavaScript
+let decimalMultiply = 2.0 * 2.5;
+console.log("decimal multiply number : "+decimalMultiply);
+
+//question 18 is Divide One Decimal by Another with JavaScript
+let decimaldivide = 4.4 / 2.0;
+console.log("divide in decimal : "+decimaldivide);
+
+//question 19 is Finding a Remainder in JavaScript
+let remainder = 11%3;
+console.log("remainder value : "+remainder);
+
+//question 20 is Compound Assignment With Augmented Addition
+let num1 = 3;
+let num2 = 17;
+let num3 = 12;
+num1 +=12;
+num2 +=9;
+num3 += 7;
+console.log(num1,num2,num3);
+
+
+
+
+
+
+
