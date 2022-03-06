db20ff96641c082e18f54cf05b0d91b4d2cc54d0
preetirajput <rajput.preeti.singh@gmail.com>
2022-03-06 16:10:47 +0530

    question2 is done

--- question1.js
+++ question1.js
@@ -1,4 +1,8 @@
-//comment your js code
+//question 1 is comment your js code
 /*let a=3;
 console .log(a);*/
 console.log("single line and multi line comment is done");
+ 
+//question 2 is Declare JavaScript Variables
+let num = 5;
+console.log(num);
